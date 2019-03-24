0244c1808658f150c99ca5b048e7cd510bf46796
Henrique Joaquim <42215737+concrete-kite@users.noreply.github.com>
2019-03-24 18:34:49 -0300

    Add files via upload

---
+++ hjfcn.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int printQueue(); 
+int hash();
+void show();
+
+struct head{
+    struct node * first;
+    int size;
+    struct node * last;
+};
+
+struct node{
+    int data;
+    struct node * link;
+};
+
+int main() {
+
+    int iter;
+    int mod;
+    int size;
+    int* keys;
+
+    scanf("%d", &iter);
+
+
+    for(int i = 0; i < iter ; i++){
+
+        int* keys;
+
+        scanf("%d %d", &mod, &size);
+
+        keys = malloc(sizeof(int) * size);
+
+
+        for(int j = 0; j < size ; j++){
+            scanf("%d", &keys[j]);
+        }
+
+
+
+        struct head * roots = (struct head *)malloc(sizeof(struct head) * mod);
+
+        printf("hello there\n");
+
+        for (int j = 0; j < size; ++j){
+            
+            struct node * newnode;
+            newnode = (struct node *)malloc(sizeof(struct node));
+
+            printf("hello there 2\n");
+
+            newnode->data = keys[j];
+            int pos = hash(mod, keys[j]);
+
+            if(roots[pos].first == NULL){
+                printf("hello there 3\n");
+                roots[pos].first = newnode;
+                roots[pos].last = newnode;
+
+            }else{
+                printf("hello there 4\n");
+                roots[pos].last->link = newnode;
+                roots[pos].last = newnode;
+
+            }
+
+        }
+
+        for (int j = 0; j < mod; j++){
+            printf("%d ->", j);
+            printQueue(roots[j].first);
+        }
+
+
+
+    }
+
+
+    return 0;
+
+}
+
+
+int printQueue(struct node * root){
+
+    struct node * temp = root;
+
+    while(temp != NULL){
+        printf("%d -> ",temp->data);
+        temp = temp->link;
+    }
+    printf("\\ \n");
+    return 1;
+    
+
+}
+
+
+int hash(int modulator, int key){
+	int address;
+	address = key % modulator;
+	return address;
+}
+
+
+
